e9600eb2d36bcbb40a75d17f2f4b630facb340a7
riverpadilla <riverpadilla@msn.com>
2023-03-05 15:49:31 -0500

    Se crean clases requeridas para crear y autenticar el token de JWT

---
+++ src/main/java/tech/rivernet/BackEnd/security/JWT/JwtProvider.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tech.rivernet.BackEnd.security.JWT;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import tech.rivernet.BackEnd.security.MainUser;
+
+import java.util.Date;
+
+/**
+ *
+ * @author river
+ */
+
+@Component
+public class JwtProvider {
+   
+    private static  final Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    public String generateToken(Authentication authentication) {
+        MainUser usuarioPrincipal = (MainUser) authentication.getPrincipal();
+        return Jwts.builder().setSubject(usuarioPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String getNombreUsuarioFromToken(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("token mal formado " +e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("token no soportado " +e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("token expirado " +e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("token vacío " +e.getMessage());
+        } catch (SignatureException e) {
+            logger.error("error en la firma " +e.getMessage());
+        }
+        return false;
+    }
+    
+}
---
+++ src/main/java/tech/rivernet/BackEnd/security/JWT/JwtTokenFilter.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tech.rivernet.BackEnd.security.JWT;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+import tech.rivernet.BackEnd.service.UserDetailsServiceImpl;
+
+/**
+ *
+ * @author river
+ */
+public class JwtTokenFilter extends OncePerRequestFilter {
+    
+    private static  final Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = getToken(req);
+            if(token !=null && jwtProvider.validateToken(token)){
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }catch (Exception e){
+            logger.error("fail en método doFilter " + e.getMessage());
+        }
+        filterChain.doFilter(req, res);
+    }
+
+    private String getToken(HttpServletRequest request){
+        String authReq = request.getHeader("Authorization");
+        if(authReq != null && authReq.startsWith("Bearer "))
+            return authReq.replace("Bearer ", "");
+        return null;
+    }
+    
+}
